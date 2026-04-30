13ec4b9b91599a685093015928b7e680a01aed99
joselucenaf <joselucenafn@gmail.com>
2026-04-30 15:47:03 -0300

    Startando o projeto

---
+++ src/main/java/com/Lucena/Usuario_Reservas/UsuarioReservasApplication.java
@@ -0,0 +1,13 @@
+package com.Lucena.Usuario_Reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioReservasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Lucena/Usuario_Reservas/UsuarioReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lucena.Usuario_Reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
